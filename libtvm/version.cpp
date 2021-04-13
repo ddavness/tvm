db0b78212ce4da258c62f845d3dea29d9885a754
David Duque <david@duqued.net>
2021-04-13 09:54:33 +0100

    Initialize CMake and source files

---
+++ libtvm/version.cpp
@@ -0,0 +1,21 @@
+#include "version.hpp"
+#include <string>
+
+using std::string;
+using std::to_string;
+
+int16_t tvm::version_major() {
+    return TVM_VERSION_MAJOR;
+}
+
+int16_t tvm::version_minor() {
+    return TVM_VERSION_MINOR;
+}
+
+int16_t tvm::version_patch() {
+    return TVM_VERSION_PATCH;
+}
+
+string tvm::version() {
+    return to_string(TVM_VERSION_MAJOR) + '.' + to_string(TVM_VERSION_MINOR) + '.' + to_string(TVM_VERSION_PATCH);
+}
