78e539d70efac59faab9fe4e312aa77876530845
David Duque <david@duqued.net>
2021-04-17 19:13:03 +0100

    libtvm/windows: Export the functions provided via __declspec

--- libtvm/version.cpp
+++ libtvm/version.cpp
@@ -1,21 +1,23 @@
+#include "win32-dll.h"
+
 #include "version.hpp"
 #include <string>
 
 using std::string;
 using std::to_string;
 
-int16_t tvm::version_major() {
+PUB int16_t tvm::version_major() {
     return TVM_VERSION_MAJOR;
 }
 
-int16_t tvm::version_minor() {
+PUB int16_t tvm::version_minor() {
     return TVM_VERSION_MINOR;
 }
 
-int16_t tvm::version_patch() {
+PUB int16_t tvm::version_patch() {
     return TVM_VERSION_PATCH;
 }
 
-string tvm::version() {
+PUB string tvm::version() {
     return to_string(TVM_VERSION_MAJOR) + '.' + to_string(TVM_VERSION_MINOR) + '.' + to_string(TVM_VERSION_PATCH);
 }
