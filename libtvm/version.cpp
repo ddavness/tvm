bc6ea45210f42519696024d038287f639f1b02b0
David Duque <david@duqued.net>
2021-05-03 11:49:43 +0100

    Get rid of the win32-dll header - not required anymore

--- libtvm/version.cpp
+++ libtvm/version.cpp
@@ -1,23 +1,22 @@
-#include "win32-dll.h"
-
 #include "version.hpp"
+
 #include <string>
 
 using std::string;
 using std::to_string;
 
-PUB int16_t tvm::version_major() {
+int16_t tvm::version_major() {
     return TVM_VERSION_MAJOR;
 }
 
-PUB int16_t tvm::version_minor() {
+int16_t tvm::version_minor() {
     return TVM_VERSION_MINOR;
 }
 
-PUB int16_t tvm::version_patch() {
+int16_t tvm::version_patch() {
     return TVM_VERSION_PATCH;
 }
 
-PUB string tvm::version() {
+string tvm::version() {
     return to_string(TVM_VERSION_MAJOR) + '.' + to_string(TVM_VERSION_MINOR) + '.' + to_string(TVM_VERSION_PATCH);
 }
