e1c6e1ff6fdc3ea6d1b97a6025e4785d4f1b442b
David <david@duqued.net>
2021-07-25 23:38:50 +0100

    Prepare state so that it can be used in machines

--- libtvm/state.cpp
+++ libtvm/state.cpp
@@ -8,13 +8,25 @@
 
 #include "libtvm/state.hpp"
 
+#include <functional>
 #include <string>
 
+using std::hash;
+using std::size_t;
 using std::string;
+
 using tvm::state;
 
 state::state(const string name): tag(name) {}
 
-const string& state::name() {
+const string& state::name() const {
     return tag;
+}
+
+bool state::operator==(const state& other) const {
+    return name() == other.name();
+}
+
+size_t hash<state>::operator()(const tvm::state& state) const {
+    return hash<string>()(state.tag);
 }
--- libtvm/state.hpp
+++ libtvm/state.hpp
@@ -4,16 +4,23 @@
 
 #pragma once
 
+#include <cstddef>
+#include <functional>
 #include <string>
 
 namespace tvm {
     class state {
       private:
-        const std::string tag;
+        std::string tag;
+        friend struct std::hash<tvm::state>;
 
       public:
         state(std::string);
 
-        [[nodiscard]] const std::string& name();
+        [[nodiscard]] const std::string& name() const;
+
+        bool operator==(const tvm::state&) const;
     };
 } // namespace tvm
+
+template<> struct std::hash<tvm::state> { std::size_t operator()(const tvm::state&) const; };
